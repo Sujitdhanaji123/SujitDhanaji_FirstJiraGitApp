705adb5c0e03ce03a5bd5f567abff16affed05d6
Sujitdhanaji123 <dhanajisubhadranandan123@gmail.com>
2023-09-23 12:37:50 +0530

    FIR-1 : - Create Project in Spring Boot

---
+++ FirstJiraGitApp/src/test/java/com/BikkadIT/Demo/FirstJiraGitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJiraGitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
